fe314d4a1c14b363cc1d8128c65eca93659974b4
Coffee Pub <drowbe@yhoo.com>
2025-08-22 17:46:45 -0700

    chore: update module.json for version 12.1.0, enhance author details, and adjust module relationships

--- scripts/const.js
+++ scripts/const.js
@@ -2,6 +2,19 @@
 // ===== EXPORTS ====================================================
 // ================================================================== 
 
+// Import module.json
+import moduleData from '../module.json' assert { type: 'json' };
+
+export const MODULE = {
+    ID: moduleData.id, // coffee-pub-crier
+    NAME: 'CRIER', // CRIER or moduleData.title.toUpperCase().replace(/\s+/g, '_')
+    TITLE: moduleData.title, // Coffee Pub Crier
+    AUTHOR: moduleData.authors[0]?.name || 'COFFEE PUB',
+    VERSION: moduleData.version,
+    DESCRIPTION: moduleData.description 
+};
+
+// MIGRATION - we need to transition these to the new MODULE object
 export const MODULE_TITLE = 'CRIER'
 export const MODULE_ID = 'coffee-pub-crier'
 export const CRIER = {
